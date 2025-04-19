9d1dc1e5d85cad4dc5f1c42c0994552205a2ba5d
m4tyu <150413157+m4tyu@users.noreply.github.com>
2025-04-19 18:53:17 +0800

    main

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import dash
 import dash_bootstrap_components as dbc
 from components.header import create_header
-from components.main import create_main_content
-from callbacks import register_callbacks
+from components.overview import create_main_content
 
 # Initialize the app
 app = dash.Dash(
     __name__, 
     external_stylesheets=[dbc.themes.BOOTSTRAP],
-    assets_folder='assets'  # Make sure you have an assets folder for storing images
+    assets_folder='assets' 
 )
 
 # Assemble the layout
@@ -20,9 +19,6 @@ app.layout = dash.html.Div(
     ],
     style={"margin": "0", "padding": "0", "background": "#f5f5f5"}
 )
-
-# Register all callbacks
-register_callbacks(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
