78e86cc2d70035abeca916218792b97d1336d1e5
m4tyu <150413157+m4tyu@users.noreply.github.com>
2025-04-19 19:31:45 +0800

    overview and density

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
 from components.header import create_header
-from components.overview import create_main_content
+from components.overview import create_overview_content
+# from components.density import create_density_content
+
 
 # Initialize the app
 app = dash.Dash(
@@ -15,7 +17,8 @@ app.layout = dash.html.Div(
     children=[
         create_header(),
         dash.html.Div(id="menu-output", style={"margin-top": "20px", "font-size": "18px"}),
-        create_main_content(),
+        create_overview_content(),
+        # create_density_content(),
     ],
     style={"margin": "0", "padding": "0", "background": "#f5f5f5"}
 )
