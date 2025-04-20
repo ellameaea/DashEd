0844886972bca1db1aefd52f84ab529d17fb3a23
kim <kimberlytisip@gmail.com>
2025-04-21 04:02:48 +0800

    added 'number of schools by region and modified coc' graph

--- components/density.py
+++ components/density.py
@@ -6,6 +6,8 @@ import numpy as np
 import json
 import os
 from components.overview import create_info_card, create_two_column_layout
+from Data_Visualization.Enrollee_and_School_Analysis.modified_COCs_count import fig
+
 
 
 def create_density_content():
@@ -14,7 +16,7 @@ def create_density_content():
     # Create charts
     card1_content = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.")
     card2_content = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.")
-    card3_content = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.")
+    card3_content = html.Div(dcc.Graph(id='stacked-bar-chart', figure=fig, config={'responsive': True},style={"width": "1215", "height": "100%"}),style={"height": "auto"})
 
 
     # Create components
@@ -22,7 +24,7 @@ def create_density_content():
     card1 = create_info_card("Title for Data Viz 2", card2_content, height = 820)
 
     main_section = create_two_column_layout(density_main, card1)
-    bottom_section = create_info_card("Title for Data Viz 3", card3_content, width= 1355)
+    bottom_section = create_info_card("Number of Schools by Region and Modified COC", card3_content, width="1215", height=None)
     
     # Combine all components
     return html.Div([
