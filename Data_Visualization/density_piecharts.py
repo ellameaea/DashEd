c9d5942523c8ce4bd6c2ac94b4cdc8bfdd3fec5c
kim <kimberlytisip@gmail.com>
2025-04-21 14:19:15 +0800

    added 2 pie charts

---
+++ Data_Visualization/density_piecharts.py
@@ -0,0 +1,131 @@
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import pandas as pd
+from io import BytesIO
+import base64
+import matplotlib.pyplot as plt
+
+# Create figure for Public Pie Chart
+def public_pie_chart():
+    # Import and preprocess data from Public_PieChart.py
+    df = pd.read_csv('CSV Files/CLEANED_SY2023_Enrollment.csv')
+    public_schools = df[df['Sector'].isin(['Public', 'SUCsLUCs'])]
+
+    combined_levels = {
+        'Kindergarten': ['K Male', 'K Female'],
+        'ELEM': ['G1 Male', 'G1 Female', 'G2 Male', 'G2 Female', 'G3 Male', 'G3 Female', 'G4 Male', 'G4 Female', 'G5 Male', 'G5 Female', 'G6 Male', 'G6 Female', 'Elem NG Male', 'Elem NG Female'],
+        'JHS': ['G7 Male', 'G7 Female', 'G8 Male', 'G8 Female', 'G9 Male', 'G9 Female', 'G10 Male', 'G10 Female', 'JHS NG Male', 'JHS NG Female'],
+        'SHS': ['G11 ACAD ABM Male', 'G11 ACAD ABM Female', 'G11 ACAD HUMSS Male', 'G11 ACAD HUMSS Female', 'G11 ACAD STEM Male', 'G11 ACAD STEM Female', 'G11 ACAD GAS Male', 'G11 ACAD GAS Female', 'G11 ACAD PBM Male', 'G11 ACAD PBM Female', 'G11 TVL Male', 'G11 TVL Female', 'G11 SPORTS Male', 'G11 SPORTS Female', 'G11 ARTS Male', 'G11 ARTS Female', 'G12 ACAD ABM Male', 'G12 ACAD ABM Female', 'G12 ACAD HUMSS Male', 'G12 ACAD HUMSS Female', 'G12 ACAD STEM Male', 'G12 ACAD STEM Female', 'G12 ACAD GAS Male', 'G12 ACAD GAS Female', 'G12 ACAD PBM Male', 'G12 ACAD PBM Female', 'G12 TVL Male', 'G12 TVL Female', 'G12 SPORTS Male', 'G12 SPORTS Female', 'G12 ARTS Male', 'G12 ARTS Female']
+    }
+    
+    all_enrollee_columns = [col for level_cols in combined_levels.values() for col in level_cols]
+    public_schools['Total Enrollees'] = public_schools[all_enrollee_columns].sum(axis=1)
+    
+    level_enrollment = {}
+    for level, columns in combined_levels.items():
+        level_enrollment[level] = public_schools[columns].sum().sum()
+        
+    level_enrollment_df = pd.DataFrame(level_enrollment.items(), columns=['Level', 'Total Enrollment'])
+    level_enrollment_df['Percentage'] = (level_enrollment_df['Total Enrollment'] / level_enrollment_df['Total Enrollment'].sum()) * 100
+
+    # Pie chart for labels outside
+    labels_trace = go.Pie(
+        labels=level_enrollment_df['Level'], 
+        values=level_enrollment_df['Percentage'], 
+        hole=0,
+        sort=False,
+        textinfo='label',  
+        textposition='outside',  
+        showlegend=True 
+    )
+
+    # Pie chart for percentages inside
+    percentages_trace = go.Pie(
+        labels=level_enrollment_df['Level'], 
+        values=level_enrollment_df['Percentage'], 
+        hole=0,
+        sort=False,
+        textinfo='percent',  
+        textposition='inside',  
+        insidetextfont=dict(color="black"), 
+        showlegend=True  
+    )
+
+    # Combine the two traces
+    fig = go.Figure(data=[labels_trace, percentages_trace])
+    
+    fig.update_layout(title="", height=390, legend_title_text="Grade Levels", legend=dict(
+        title="Grade Levels",  
+        borderwidth=1,  
+        bordercolor="lightgray",  
+        traceorder="normal",  
+        orientation="v",  
+        y=0.5,  
+        yanchor="middle",  
+        x=1.2,  
+        font=dict(size=10)
+    ))
+    return fig
+
+
+# Create figure for Private Pie Chart
+def private_pie_chart():
+    # Import and preprocess data from Private_PieChart.py
+    df = pd.read_csv('CSV Files/CLEANED_SY2023_Enrollment.csv')
+    private_schools = df[df['Sector'].isin(['Private', 'PSO'])]
+
+    combined_levels = {
+        'Kindergarten': ['K Male', 'K Female'],
+        'ELEM': ['G1 Male', 'G1 Female', 'G2 Male', 'G2 Female', 'G3 Male', 'G3 Female', 'G4 Male', 'G4 Female', 'G5 Male', 'G5 Female', 'G6 Male', 'G6 Female', 'Elem NG Male', 'Elem NG Female'],
+        'JHS': ['G7 Male', 'G7 Female', 'G8 Male', 'G8 Female', 'G9 Male', 'G9 Female', 'G10 Male', 'G10 Female', 'JHS NG Male', 'JHS NG Female'],
+        'SHS': ['G11 ACAD ABM Male', 'G11 ACAD ABM Female', 'G11 ACAD HUMSS Male', 'G11 ACAD HUMSS Female', 'G11 ACAD STEM Male', 'G11 ACAD STEM Female', 'G11 ACAD GAS Male', 'G11 ACAD GAS Female', 'G11 ACAD PBM Male', 'G11 ACAD PBM Female', 'G11 TVL Male', 'G11 TVL Female', 'G11 SPORTS Male', 'G11 SPORTS Female', 'G11 ARTS Male', 'G11 ARTS Female', 'G12 ACAD ABM Male', 'G12 ACAD ABM Female', 'G12 ACAD HUMSS Male', 'G12 ACAD HUMSS Female', 'G12 ACAD STEM Male', 'G12 ACAD STEM Female', 'G12 ACAD GAS Male', 'G12 ACAD GAS Female', 'G12 ACAD PBM Male', 'G12 ACAD PBM Female', 'G12 TVL Male', 'G12 TVL Female', 'G12 SPORTS Male', 'G12 SPORTS Female', 'G12 ARTS Male', 'G12 ARTS Female']
+    }
+
+    all_enrollee_columns = [col for level_cols in combined_levels.values() for col in level_cols]
+    private_schools['Total Enrollees'] = private_schools[all_enrollee_columns].sum(axis=1)
+    
+    level_enrollment = {}
+    for level, columns in combined_levels.items():
+        level_enrollment[level] = private_schools[columns].sum().sum()
+        
+    level_enrollment_df = pd.DataFrame(level_enrollment.items(), columns=['Level', 'Total Enrollment'])
+    level_enrollment_df['Percentage'] = (level_enrollment_df['Total Enrollment'] / level_enrollment_df['Total Enrollment'].sum()) * 100
+
+    # Pie chart for labels outside
+    labels_trace = go.Pie(
+        labels=level_enrollment_df['Level'], 
+        values=level_enrollment_df['Percentage'], 
+        hole=0,
+        sort=False,
+        textinfo='label',  
+        textposition='outside',  
+        showlegend=False 
+    )
+
+    # Pie chart for percentages inside
+    percentages_trace = go.Pie(
+        labels=level_enrollment_df['Level'], 
+        values=level_enrollment_df['Percentage'], 
+        hole=0,
+        sort=False,
+        textinfo='percent',  
+        textposition='inside',  
+        insidetextfont=dict(color="black"),  
+        showlegend=True  
+    )
+
+    # Combine the two traces
+    fig = go.Figure(data=[labels_trace, percentages_trace])
+    
+    fig.update_layout(title="", height=390, legend_title_text="Grade Levels", legend=dict(
+        title="Grade Levels",  
+        borderwidth=1,  
+        bordercolor="lightgray", 
+        traceorder="normal", 
+        orientation="v",  
+        y=0.5,  
+        yanchor="middle",
+        x=1.2,  
+        font=dict(size=10)
+    ))
+    return fig
--- components/density.py
+++ components/density.py
@@ -7,21 +7,26 @@ import json
 import os
 from components.overview import create_info_card, create_two_column_layout
 from Data_Visualization.Enrollee_and_School_Analysis.modified_COCs_count import fig
+from Data_Visualization.density_piecharts import public_pie_chart, private_pie_chart
 
-
+# Path to the saved pie chart images
+public_pie_chart_path = 'assets/public_pie_chart.png'
+private_pie_chart_path = 'assets/private_pie_chart.png'
 
 def create_density_content():
     """Create the main dashboard content using reusable components"""
     
     # Create charts
     card1_content = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.")
-    card2_content = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.")
+    card2_content = html.Div([
+    html.Div(dcc.Graph(id='private-pie-chart', figure=private_pie_chart(), config={'responsive': True}, style={"height": "50%"})),
+    html.Div(dcc.Graph(id='public-pie-chart', figure=public_pie_chart(), config={'responsive': True}, style={"height": "50%"}))], style={"max-height": "820px", "display": "flex", "flexDirection": "column"})
     card3_content = html.Div(dcc.Graph(id='stacked-bar-chart', figure=fig, config={'responsive': True},style={"width": "1215", "height": "100%"}),style={"height": "auto"})
 
 
     # Create components
     density_main = create_info_card("Title for Data Viz 1", card1_content, height=820)
-    card1 = create_info_card("Title for Data Viz 2", card2_content, height = 820)
+    card1 = create_info_card("", card2_content, height = 820)
 
     main_section = create_two_column_layout(density_main, card1)
     bottom_section = create_info_card("Number of Schools by Region and Modified COC", card3_content, width="1215", height=None)
