1d5505141ae61a2bdeb0e99e519fca8b9ba2c5dc
kim <kimberlytisip@gmail.com>
2025-04-22 01:20:04 +0800

    fixed the width of the bottom section in the density

--- components/density.py
+++ components/density.py
@@ -18,21 +18,29 @@ def create_density_content():
     
     # Create charts
     card1_content = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.")
-    card2_content = html.Div([
-    html.Div(dcc.Graph(id='private-pie-chart', figure=private_pie_chart(), config={'responsive': True}, style={"height": "50%"})),
-    html.Div(dcc.Graph(id='public-pie-chart', figure=public_pie_chart(), config={'responsive': True}, style={"height": "50%"}))], style={"max-height": "820px", "display": "flex", "flexDirection": "column"})
-    card3_content = html.Div(dcc.Graph(id='stacked-bar-chart', figure=fig, config={'responsive': True},style={"width": "1215", "height": "100%"}),style={"height": "auto"})
+    card2_content = html.Div([ 
+        html.Div(dcc.Graph(id='private-pie-chart', figure=private_pie_chart(), config={'responsive': True}, style={"height": "50%"})),
+        html.Div(dcc.Graph(id='public-pie-chart', figure=public_pie_chart(), config={'responsive': True}, style={"height": "50%"}))
+    ], style={"max-height": "820px", "display": "flex", "flexDirection": "column"})
 
-
+    card3_content = html.Div(dcc.Graph(id='stacked-bar-chart', figure=fig, config={'responsive': True}, style={"height": "100%"}), style={"height": "auto"})
+    
     # Create components
     density_main = create_info_card("Title for Data Viz 1", card1_content, height=820)
     card1 = create_info_card("", card2_content, height = 820)
 
     main_section = create_two_column_layout(density_main, card1)
-    bottom_section = create_info_card("Number of Schools by Region and Modified COC", card3_content, width="1215", height=None)
-    
+    bottom_section = create_info_card("Number of Schools by Region and Modified COC", card3_content,width="100%", height=None)
+
     # Combine all components
     return html.Div([
         main_section,
         bottom_section
-    ], style={"max-width": "1400px", "margin": "0 auto", "padding": "20px"})
+    ], style={
+        "max-width": "1400px", 
+        "margin": "0 auto", 
+        "padding": "20px", 
+        "display": "flex",  # Add flex display here
+        "flexDirection": "column",  # Ensure vertical layout
+        "boxSizing": "border-box"
+    })
